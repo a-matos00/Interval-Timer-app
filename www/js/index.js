5409d60965e46f652a206a452a0507ddb03fc715
AndrijaM <andrija.matosic@outlook.com>
2020-12-22 12:50:19 +0100

    Learned how to use local storage

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
@@ -26,4 +9,59 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
+
 }
+
+
+
+document.body.onload = function load()
+{
+    x = new display_screen;
+    document.getElementById("box").appendChild(x.display);
+    y = new fbutton("btn");
+    document.getElementById("box").appendChild(y.el);
+    
+
+    if( localStorage.getItem("broj") != null || NaN || undefined)
+    {      
+        $("#box").append(localStorage.getItem("broj"));
+    }
+    else{
+        document.getElementById("box").innerHTML += "nema";
+    }
+    
+    
+}
+
+
+class display_screen
+{   
+    constructor()
+    {
+        this.display = document.createElement("DIV");
+        this.display.classList.add("display");
+        
+    }
+    
+}
+
+class fbutton
+{   
+    constructor(arg_id)
+    {
+        this.el = document.createElement("BUTTON");
+        this.el.innerHTML = "botun";
+        this.el.classList.add("fbutton");
+        this.el.id = arg_id;
+
+        $(this.el).click(function(){
+            localStorage.setItem("broj", "2");
+            $("#btn").html(localStorage.getItem("broj"));
+        });
+            
+     }   
+}
+
+
+
